24c644e73430dd8b435b317acfad36f2157c76de
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-20 14:38:34 +0600

    sadasdasd

--- install-binary-commands.sh
+++ install-binary-commands.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/cache/delevery-updates/ggs/caches/updates/binary-commands
-cp ./binary-commands /usr/bin
+cd  /var/cache/delevery-updates/ggs/caches/updates/binary-commands
+cp -R ./binary-commands /usr/bin
